ee1bd1937c28e041e857356d3d2856fcb185181e
Drake Eckles <drake.eckles@lsr7.net>
2016-09-08 17:55:04 +0000

    Changes made by Drake Eckles (drake.eckles@lsr7.net).

--- LSR7App/components/lunch/index.js
+++ LSR7App/components/lunch/index.js
@@ -7,5 +7,4 @@ app.lunch = kendo.observable({
 
 // START_CUSTOM_CODE_lunch
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
-
 // END_CUSTOM_CODE_lunch
