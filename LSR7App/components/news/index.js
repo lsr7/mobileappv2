86d04b201617af7b919dd752264d5d758a11613f
Rusty Richards <rusty.richards@lsr7.net>
2017-07-29 21:37:55 +0000

    Changes made by Rusty Richards (rusty.richards@lsr7.net).

---
+++ LSR7App/components/news/index.js
@@ -0,0 +1,46 @@
+(function (global) {
+    var NewsViewModel,
+        app = global.app = global.app || {};
+
+    NewsViewModel = kendo.data.ObservableObject.extend({
+        NewsDataSource: null,
+
+        init: function () {
+            var that = this,
+                dataSource,
+                jsonUrlToLoad;
+
+            kendo.data.ObservableObject.fn.init.apply(that, []);
+
+            //When you build for Apache Cordova 3.0.0, apply this code instead of using relative URLs. In Apache Cordova 3.0.0, relative URLs might not work properly.
+            //jsonUrlToLoad = app.makeUrlAbsolute("data/News.json");
+            jsonUrlToLoad = "https://www1.lsr7.org/mobile_app/news.json";
+
+            dataSource = new kendo.data.DataSource({
+                transport: {
+                    read: {
+                        url: jsonUrlToLoad,
+                        dataType: "json"
+                    }
+                },
+            schema: {
+            data: "Result",
+             model: {
+                    fields: {
+                        'TITLE': {
+                            field: 'TITLE',
+                            defaultValue: ''
+                        },
+                    }
+                }
+            }
+            });
+ 
+            that.set("NewsDataSource", dataSource);
+        }
+    });
+
+    app.newsService = {
+        viewModel: new NewsViewModel()
+    };
+})(window);
