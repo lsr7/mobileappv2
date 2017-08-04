4b35855f515878828c4db3350987c547b7f4fbd6
Rusty Richards <rusty.richards@lsr7.net>
2017-08-04 18:21:49 +0000

    Changes made by Rusty Richards (rusty.richards@lsr7.net).

--- LSR7App/dataProviders/jsonDataProvider3.js
+++ LSR7App/dataProviders/jsonDataProvider3.js
@@ -2,7 +2,7 @@
 
 (function() {
     app.data.jsonDataProvider3 = {
-        url: 'https://d2oqpg9vq3df92.cloudfront.net/mobile/Other_schools.json'
+        url: 'https://d2oqpg9vq3df92.cloudfront.net/mobile/other_schools.json'
     }
 }());
 
